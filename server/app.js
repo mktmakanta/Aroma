e09fe0201cd3f75f9598c3e18323ed338404b8c2
mktmakanta <mktmakantamkt@gmail.com>
2025-09-13 04:54:52 +0100

    Finished with the Auth section

--- server/app.js
+++ server/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-// const hpp = require('hpp');
+const hpp = require('hpp');
+const expressMongoSanitize = require('@exortek/express-mongo-sanitize');
+const { xss } = require('express-xss-sanitizer');
 
 const AppError = require('./utils/appError');
 const rateLimit = require('express-rate-limit');
@@ -22,6 +22,8 @@ const app = express();
 
 // ----------MIDDLEWARES-------------
 // These middlewares are arranged hierrachically according to best practice and use
+app.use(cors());
+
 app.use(
   helmet({
     contentSecurityPolicy: false, // disable CSP if not needed
@@ -31,8 +33,6 @@ app.use(
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
-app.use(cors());
 
 const limiter = rateLimit({
   max: 100,
@@ -45,14 +45,12 @@ app.use('/api', limiter);
 
 app.use(express.json({ limit: '10kb' })); // parse requests
 
-// Data sanitization against NoSQL query injection
-app.use(mongoSanitize());
+app.use(expressMongoSanitize()); // https://github.com/ExorTek/express-mongo-sanitize || older version is throwing error
 
-// Sanitize against XSS/ cross side scripting
 app.use(xss());
 
 // prevent parameter pollutions
-// app.use(hpp());
+app.use(hpp());
 
 // To serve static files
 app.use(express.static(`${__dirname}/public`));
